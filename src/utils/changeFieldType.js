debcd4d4c68cfb6fc3bbc5b9b222ef5e5f1f02c7
Racheal Pennell <racheal.larimer@gmail.com>
2022-01-13 15:05:08 -0800

    add changeFieldType script

--- src/utils/changeFieldType.js
+++ src/utils/changeFieldType.js
@@ -10,33 +10,35 @@ let queue = cq().limit({ concurrency: 2 }).process(function (task) {
 })
 
 //We write our query for the document(s) we want to change fields on
-const query = `*[_id == 'allInputExamples']`
+const query = `*[_type == 'person' && image._type == 'image']`
 
 //Create an object that contains the fields and values you'd like to write
-const mutation = {
-  title: 'Custom Input Examples'
-}
 
 const addField = async () => {
   // Use the configured Studio client to fetch our documents
   const docs = await studioClient.fetch(query)
   // Loop through all of the docs returned from our query
   for (const doc of docs){
-    queue(doc).then(async () => {
     // Add a message to help us know the upload is happening
-      console.log('Uploading...')
-      // Tell the client to patch the current document
-      studioClient.patch(doc._id)
-        // Set the fields we specified in our mutation
-        .set(mutation)
-        // Commit the changes
-        .commit()
-        .then(updatedDoc => {
-          console.log(`Hurray, the doc is updated! New document:${updatedDoc._id}`)
-        })
-        .catch((err) => {
-          console.error('Oh no, the update failed: ', err.message)
-        })
+    queue(doc).then(async () => {
+    console.log('Uploading...')
+    // Tell the client to patch the current document
+    studioClient.patch(doc._id)
+      // Set the fields we specified in our mutation
+      .set({
+        image: {
+          _type: 'figure',
+          asset: doc.image.asset
+        }
+      })
+      // Commit the changes
+      .commit()
+      .then(updatedDoc => {
+        console.log(`Hurray, the doc is updated! New document:${updatedDoc._id}`)
+      })
+      .catch((err) => {
+        console.error('Oh no, the update failed: ', err.message)
+      })
     })
   }
 }
